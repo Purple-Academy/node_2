b37546a7236ae2b61fd444a38941dce8acbd8658
datokhojava <khojavad30@gmail.com>
2024-11-26 16:45:08 +0400

    post new article controller

--- controllers.js
+++ controllers.js
@@ -11,7 +11,7 @@ function getAllArticles(req, res) {
     res.end(JSON.stringify(articles));
   } catch (error) {
     res.statusCode = 401;
-    res.end("No such entity");
+    res.end(`No such entity: ${error}`);
   }
 }
 
@@ -24,26 +24,37 @@ function getArticle(req, res, params) {
     try {
       const article = mock.find((items) => items.id === id);
 
-      res.statusCode = 200
+      res.statusCode = 200;
       res.end(JSON.stringify(article));
     } catch (error) {
-     res.statusCode = 401;
-     res.end("No such record");
+      res.statusCode = 401;
+      res.end(`No such record: ${error}`);
     }
   });
 }
 
 function postArticle(req, res, payload, cb) {
-  helper.parseBody(req, (err, body) => {
-    const newItem = {
-      id: news.length + 1,
-      text: body.text,
-      date: body.date,
-    };
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
 
-    news.push(newItem);
-    res.statusCode = 201;
-    res.end("Created");
+  helper.parseBody(req, (err, body) => {
+    console.log(body);
+
+    try {
+      const newArticle = {
+        id: mock.length + 1,
+        title: body.title,
+        text: body.text,
+        date: 231,
+        author: body.author,
+      };
+
+      mock.push(newArticle);
+      res.statusCode = 201;
+      res.end("Created");
+    } catch (error) {
+      res.statusCode = 401;
+      res.end(`No such record: ${error}`);
+    }
   });
 }
 
