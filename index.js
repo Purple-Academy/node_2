1090a52e1fc3a26542134e9f082e8308f8600154
datokhojava <khojavad30@gmail.com>
2024-11-26 14:17:24 +0400

    updated index

--- index.js
+++ index.js
@@ -1,107 +1,39 @@
 const http = require("http");
+const controllers = require("./controllers");
+const helper = require("./helper");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const handlers = {
-  "/sum": sum,
-  "/mult": mult,
-  //new routes
-  "/api/articles/readall": getAllArticles,
-  "/api/articles/read": getArticle,
-  "/api/articles/create": postArticle,
-  "/api/articles/update": updateArticle,
-  "/api/articles/delete": deleteArticle,
-  "/api/comments/create": postComment,
-  "/api/comments/delete": deleteComment,
+const endpointMapper = {
+  "/api/articles": controllers.getAllArticles,
+  "/api/articles/read": controllers.getArticle,
+  "/api/articles/create": controllers.postArticle,
+  "/api/articles/update": controllers.updateArticle,
+  "/api/articles/delete": controllers.deleteArticle,
+  "/api/comments/create": controllers.postComment,
+  "/api/comments/delete": controllers.deleteComment,
 };
 
-const server = http.createServer((req, res) => {
-  parseBodyJson(req, (err, payload) => {
-    const handler = getHandler(req.url);
+function handler(req, res) {
+  const { url, params } = helper.parseUrl(req.url);
 
-    handler(req, res, payload, (err, result) => {
-      if (err) {
-        res.statusCode = err.code;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify(err));
+  const handler = endpointMapper[url];
 
-        return;
-      }
+  if (handler) {
+    handler(req, res, params);
+  } else {
+    send404(req, res);
+  }
+}
 
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(result));
-    });
-  });
-});
+const server = http.createServer(handler);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-function getHandler(url) {
-  return handlers[url] || notFound;
-}
-
-function sum(req, res, payload, cb) {
-  const result = { c: payload.a + payload.b };
-
-  cb(null, result);
-}
-
-//added new function
-function mult(req, res, payload, cb) {
-  const result = { c: payload.a * payload.b };
-
-  cb(null, result);
-}
-
-function getAllArticles(req, res, payload, cb) {
-  console.log("get all routes");
-}
-
-function getArticle(req, res, payload, cb) {
-  console.log("1");
-}
-
-function postArticle(req, res, payload, cb) {
-  console.log("1");
-}
-
-function updateArticle(req, res, payload, cb) {
-  console.log("1");
-}
-
-function deleteArticle(req, res, payload, cb) {
-  console.log("1");
-}
-
-function postComment(req, res, payload, cb) {
-  console.log("1");
-}
-
-function deleteComment(req, res, payload, cb) {
-  console.log("1");
-}
-
-// end of routes
-function notFound(req, res, payload, cb) {
-  cb({ code: 404, message: "Not found" });
-}
-
-function parseBodyJson(req, cb) {
-  let body = [];
-
-  req
-    .on("data", function (chunk) {
-      body.push(chunk);
-    })
-    .on("end", function () {
-      body = Buffer.concat(body).toString();
-
-      let params = JSON.parse(body);
-
-      cb(null, params);
-    });
+function send404(req, res) {
+  res.statusCode = 404;
+  res.end("404 Page Not Found");
 }
